11cc3d6ed5a2ebf27c420690c95c628f71430592
cristianfatec <cristian.nascimento@fatec.sp.gov.br>
2024-11-29 22:34:05 -0300

    (29/11/24 - open api)

--- config/routes.js
+++ config/routes.js
@@ -1,24 +1,9 @@
-'use strict';
+const aluno = require('../api/v1/alunos/aluno-routes');
+const produto = require('../api/v1/produtos/produto.routes');
 
-const dotenv = require('dotenv').config();  
+const routes = [
+    ...aluno,
+    ...produto
+]
 
-// All configurations will extend these options
-// ============================================
-const all = {
-    basePath: '/cms/v1',
-
-    env: process.env.ENVIRONMENT,
-    host: process.env.HOST || '0.0.0.0',
-    port: process.env.PORT || 5000,
-
-
-    database: {
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        name: process.env.DATABASE_NAME,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD
-    }
-}
-
-module.exports = all;
+module.exports = routes;
